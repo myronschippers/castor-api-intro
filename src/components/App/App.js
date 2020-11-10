9830c4f194f46e8294560467c1f733e2970f8cf4
Myron <dubtwee@gmail.com>
2020-11-10 14:14:05 -0600

    adding styling for my application

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// CUSTOM COMPONENTS
+import Header from '../Header/Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <main className="container">
+          <h2>More Content</h2>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
